25936910472cc1afaa338bfa6870dd208eab32e3
adhirajpandey <pandey.adhiraj02@gmail.com>
2026-02-11 19:28:41 +0530

    fix(frontend/config.ts): update production extension id

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,7 +7,7 @@ const configs = {
   production: {
     API_URL: 'https://api.vidwiz.online/v2',
     GOOGLE_CLIENT_ID: '265946502927-fsb0osc7ch7d9tvu4cr30oks5q4r61pt.apps.googleusercontent.com',
-    EXTENSION_ID: 'YOUR_EXTENSION_ID_HERE', // TODO: user to replace this
+    EXTENSION_ID: 'bgiahikcnhdljbfeknfbfpdnbnkpjiop',
   },
 };
 
